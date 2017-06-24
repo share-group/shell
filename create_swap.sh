cc56aebf1b9e4b3f3c699b5c8fecfe86e783e7ef
ruanzhijun <ruan965176201@qq.com>
2017-06-24 22:51:20 +0800

    创建交换区

---
+++ create_swap.sh
@@ -0,0 +1,12 @@
+﻿#创建磁盘交换区
+
+swapfile='/home/swap'
+
+dd if=/dev/zero of=$swapfile bs=64M count=32
+mkswap $swapfile
+swapon $swapfile
+
+echo 'create swap success...'
+
+#swapoff $swapfile
+#rm -rf $swapfile
