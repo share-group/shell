3820ff739a0c4ad6b629b9c71db217b58a6b1f18
june <zruan@kalengo.com>
2017-07-06 13:46:25 +0800

    修改脚本

--- install-git.sh
+++ install-git.sh
@@ -1,5 +1,5 @@
 #linux git
-#运行例子：mkdir -p /shell && cd /shell && rm -rf install-git.sh && wget --no-cache https://raw.githubusercontent.com/ruanzhijun/share/master/shell/install-git.sh && sh install-git.sh /usr/local
+#运行例子：mkdir -p /shell && cd /shell && rm -rf install-git.sh && wget --no-cache https://raw.githubusercontent.com/share-group/shell/master/install-git.sh && sh install-git.sh /usr/local
  
 #定义本程序的当前目录
 base_path=$(pwd)
