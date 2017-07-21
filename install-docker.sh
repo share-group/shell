88bb0712b2757b469680dd9ab8f334896782f145
june <zruan@kalengo.com>
2017-07-21 09:29:24 +0800

    更新脚本

--- install-docker.sh
+++ install-docker.sh
@@ -4,7 +4,7 @@
 #定义本程序的当前目录
 base_path=$(pwd)
 
-
+#安装docker
 yum remove -y docker docker-common docker-selinux docker-engine docker-ce
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
@@ -16,4 +16,15 @@ yum install -y docker-ce
 systemctl restart docker
 docker -v
 docker info
+
+#安装基础镜像alpine
+docker pull alpine
+
+#安装私有仓库
+
+#安装容器管理rancher
+
+#开机自启动
+echo '' >> /etc/rc.local
+echo 'systemctl start docker' >> /etc/rc.local
 
