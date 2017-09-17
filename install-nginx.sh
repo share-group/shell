64fac8e522e121d63f96b1a280c4f26b3cd35fd8
june <zruan@kalengo.com>
2017-09-17 21:22:47 +0800

    升级openssl

--- install-nginx.sh
+++ install-nginx.sh
@@ -61,7 +61,7 @@ if [ ! -d $nginx_install_path/libiconv ]; then
 fi
 
 # 安装OpenSSL
-openssl='openssl-1.1.0e'
+openssl='openssl-1.1.0f'
 if [ ! -d $nginx_install_path/openssl ]; then
 	echo 'installing '$openssl' ...'
 	if [ ! -f $base_path/$openssl.tar.gz ]; then
