88a575ef6ca577cf324a797f20ab9307f365c52c
june <zruan@kalengo.com>
2017-09-17 19:10:12 +0800

    升级mysql、gradle

--- install-mysql.sh
+++ install-mysql.sh
@@ -84,7 +84,7 @@ chown mysql.mysql -R $mysql_data_path || exit
 
 #安装mysql
 echo 'installing mysql...'
-mysql='mysql-5.7.18'
+mysql='mysql-5.7.19'
 if [ ! -d $install_path/$mysql ]; then
 	if [ ! -f $base_path/$mysql.tar.gz ]; then
 		echo $mysql'.tar.gz is not exists, system will going to download it...'
