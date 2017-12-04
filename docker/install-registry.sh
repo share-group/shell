c15edbfd0492420c23512b96f516b6af7f92167f
june <zruan@kalengo.com>
2017-12-04 09:35:49 +0800

    docker仓库

---
+++ docker/install-registry.sh
@@ -0,0 +1,27 @@
+#linux registry自动安装程序 
+#运行例子：mkdir -p /shell && cd /shell && rm -rf install-registry.sh && wget --no-cache https://raw.githubusercontent.com/share-group/shell/master/docker/install-registry.sh && sh install-registry.sh /usr/local
+
+#处理外部参数
+registry_install_path=$1
+if [ ! $registry_install_path ]; then
+	echo 'error command!!! you must input registry install path...'
+	echo 'for example: sh install-registry.sh /usr/local'
+	exit
+fi
+
+#建立临时安装目录
+echo 'preparing working path...'
+install_path='/install'
+rm -rf $install_path $registry_install_path/registry
+mkdir -p $install_path
+mkdir -p $registry_install_path/registry
+
+#拉取镜像
+docker pull registry
+
+#启动docker pull registry
+docker run -d --name registry -v $registry_install_path/registry:/var/lib/registry -p 5000:5000 --restart=always --privileged=true registry
+
+#解决https的问题
+#在”/etc/docker/“目录下，创建”daemon.json“文件。在文件中写入：
+#{ "insecure-registries":["192.168.1.100:5000"] }
