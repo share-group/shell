cdf76dd8e2474e4975e9fdf8b04111c56ee9b09b
june <zruan@kalengo.com>
2017-12-04 09:36:04 +0800

    docker仓库

--- docker/install-registry.sh
+++ docker/install-registry.sh
@@ -22,6 +22,16 @@ docker pull registry
 #启动docker pull registry
 docker run -d --name registry -v $registry_install_path/registry:/var/lib/registry -p 5000:5000 --restart=always --privileged=true registry
 
+#安装harbor作为镜像管理  https://www.cnblogs.com/huangjc/p/6266564.html
+yum -y install epel-release
+yum -y install python-pip
+pip install --upgrade backports.ssl_match_hostname
+pip install --upgrade pip
+pip install docker-compose
+docker-compose --version
+
 #解决https的问题
 #在”/etc/docker/“目录下，创建”daemon.json“文件。在文件中写入：
 #{ "insecure-registries":["192.168.1.100:5000"] }
+
+#需要用户名密码：http://blog.csdn.net/dream_an/article/details/58005324
