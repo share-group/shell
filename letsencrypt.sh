bea996ca36c01121f1b2d00dbab53e57f9f1f25b
june <zruan@kalengo.com>
2018-03-18 20:24:20 +0800

    自签证书脚本模板

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -1,16 +1,22 @@
 #自签通配符证书
+cd /letsencrypt
 
 rm -rf /root/.acme.sh
-export Ali_Key="LTAI4rx2G4I3F6sx"
-export Ali_Secret="ghN1PjKHn9Eos3uGNDK8njWrtlC6Hh"
+mkdir /root/.acme.sh
 
-cd /letsencrypt
-if [ ! -f acme.sh ]; then
-	wget --no-cache https://raw.githubusercontent.com/Neilpang/acme.sh/master/acme.sh || exit
-	chmod 777 acme.sh
-fi
+#下载最新的acme.sh脚本
+rm -rf /letsencrypt/acme.sh
+wget --no-cache https://raw.githubusercontent.com/Neilpang/acme.sh/master/acme.sh || exit
+chmod 777 acme.sh
 
-sh acme.sh --issue --dns dns_ali -d api.garybros.com -d *.garybros.com --keylength 4096 --webroot /srv/ --force || exit
-rm -rf /srv/.well-known
+#强制更新一次，实时使用最新的协议
+sh acme.sh --upgrade || exit
 
+#xxxxxxxxxxx
+export Ali_Key="xxxxxxxxxxxxxxxx"
+export Ali_Secret="xxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+sh acme.sh --issue --dns dns_ali -d xxxxx.com -d *.xxxxx.com --keylength 4096 --force || exit
+openssl dhparam -out /root/.acme.sh/xxxxx.com/dhparam.xxxxx.pem 2048 || exit
+
+#重启nginx
 nginx -t && nginx -s reload
