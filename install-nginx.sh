a61c490eebd1cbc819e3f17a8b810b846122ff73
ruanzhijun <ruanzhijun@ruanzhijun.cn>
2018-12-04 09:52:14 +0800

    升级nginx

--- install-nginx.sh
+++ install-nginx.sh
@@ -1,5 +1,5 @@
 #linux nginx自动安装程序 
-#运行例子：mkdir -p /shell && cd /shell && rm -rf install-nginx.sh && wget --no-cache https://raw.githubusercontent.com/share-group/shell/master/install-nginx.sh && sh install-nginx.sh 1.14.0 /usr/local
+#运行例子：mkdir -p /shell && cd /shell && rm -rf install-nginx.sh && wget --no-cache https://raw.githubusercontent.com/share-group/shell/master/install-nginx.sh && sh install-nginx.sh 1.14.1 /usr/local
 ntpdate ntp.api.bz
  
 #定义本程序的当前目录
@@ -10,7 +10,7 @@ nginx_version=$1
 nginx_install_path=$2
 if [ ! $nginx_version ] || [ ! $nginx_install_path ]; then
 	echo 'error command!!! you must input nginx version and install path...'
-	echo 'for example: sh install-nginx.sh 1.14.0 /usr/local'
+	echo 'for example: sh install-nginx.sh 1.14.1 /usr/local'
 	exit
 fi
 
