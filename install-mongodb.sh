9a46d7b379e96db2312de74745034e538f1a2066
ruanzhijun <ruanzhijun@ruanzhijun.cn>
2018-12-13 14:11:56 +0800

    生成mongodb配置文件

--- install-mongodb.sh
+++ install-mongodb.sh
@@ -37,6 +37,79 @@ chmod 777 /usr/bin/mongo
 chmod 777 /usr/bin/mongod
 chmod 777 /usr/bin/mongodump
 
+#创建mongodb配置文件
+echo "#数据存储路径
+dbpath="$mongodb_install_path"/mongodb/data
+
+#日志存储路径
+logpath="$mongodb_install_path"/mongodb/mongodb.log
+
+#非常详细的故障排除和各种错误的诊断日志记录
+#默认0。设置为1，为在dbpath目录里生成一个diaglog.开头的日志文件，他的值如下：
+#Value    			 Setting
+#  0    		off. No logging.       				#关闭。没有记录。
+#  1    		Log write operations.  				#写操作
+#  2    		Log read operations.   				#读操作
+#  3    		Log both read and write operations. #读写操作
+#  7    		Log write and some read operations. #写和一些读操作
+#diaglog=7
+
+#pid文件路径
+pidfilepath="$mongodb_install_path"/mongodb/mongodb.pid
+
+#日志信息冗余
+verbose=false
+vvvv=false
+
+#是否追加日志
+logappend=true
+
+#是否后台运行
+fork=true
+
+#是否开启cpu使用报告
+cpu=true
+
+#是否开启目录存储模式(简单点说，就是根据数据库名创建文件夹)
+directoryperdb=true
+
+#绑定的ip地址(可以用一个逗号分隔的列表绑定多个IP地址)
+bind_ip=0.0.0.0
+
+#启动端口
+port=27017
+
+#最大连接数(最大20000)
+maxConns=100
+
+#是否强制验证客户端请求
+objcheck=true
+
+#开启journal(参考：http://www.mongoing.com/archives/3988)
+journal=true
+
+#刷写提交机制
+journalCommitInterval=100
+
+#是否开启http接口
+nohttpinterface=false
+
+#预分配方式
+noprealloc=false
+
+#慢查询时间(单位：ms)
+slowms=100
+
+#是否开启认证模式
+noauth=true
+#auth=true
+
+#mongodb主从
+#master=true
+#slave=true
+#source=slave.mongodb.com
+" > $mongodb_install_path/mongodb/mongodb.conf || exit
+
 mongod --port 27017 --dbpath $mongodb_install_path/mongodb/data --logpath --fork $mongodb_install_path/mongodb/log.log --logappend &
 echo '' >> /etc/rc.d/rc.local
 
