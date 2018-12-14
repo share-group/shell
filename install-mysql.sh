22af13216eb302787ab60fbb340d3755522ec204
ruanzhijun <ruanzhijun@ruanzhijun.cn>
2018-12-14 10:58:18 +0800

    升级cmake

--- install-mysql.sh
+++ install-mysql.sh
@@ -22,7 +22,7 @@ mkdir -p $install_path
 yum -y install libtool sed gcc gcc-c++ make net-snmp net-snmp-devel net-snmp-utils libc6-dev python-devel rsync perl bc libxslt-dev lrzsz ncurses-devel perl bzip2 unzip vim iptables-services httpd-tools socat
 
 #安装cmake
-cmake='cmake-3.12.1'
+cmake='cmake-3.13.2'
 if [ ! -d $mysql_install_path/cmake ]; then
 	echo 'installing '$cmake'...'
 	if [ ! -f $base_path/$cmake.tar.gz ]; then
