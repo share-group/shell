2b17519e616a6cff81e46822f68b11f15ffa1403
ruanzhijun <ruanzhijun@ruanzhijun.cn>
2019-04-25 09:38:54 +0800

    优化脚本

--- install-php.sh
+++ install-php.sh
@@ -130,7 +130,7 @@ if [ ! -d $php_install_path/openssl ]; then
 fi
 
 # 安装pcre
-pcre='pcre-8.42'
+pcre='pcre-8.43'
 if [ ! -d $php_install_path/pcre ]; then
 	echo 'installing '$pcre' ...'
 	if [ ! -f $base_path/$pcre.tar.gz ]; then
