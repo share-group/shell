7a74623ac5f7761c03c1ac11405f75bc37c0cf21
ruanzhijun <ruanzhijun@ruanzhijun.cn>
2019-05-27 13:48:55 +0800

    优化nginx安装脚本

--- install-nginx.sh
+++ install-nginx.sh
@@ -151,9 +151,9 @@ http {
 	gzip on;
 	gzip_min_length 10k;
 	gzip_buffers 4 16k;
-	gzip_http_version 1.0;
-	gzip_comp_level 4;
-	gzip_types text/plain application/x-javascript text/css application/xml;
+	gzip_http_version 2.0;
+	gzip_comp_level 9;
+	gzip_types text/plain application/javascript application/x-javascript text/css application/xml text/javascript application/x-httpd-php image/jpeg image/gif image/png;
 	gzip_vary on;	
 
 	server_tokens off;    
