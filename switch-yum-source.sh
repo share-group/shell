5b6c7d0bdcbc988a2cb54853adbee859b233f48a
ruanzhijun <ruanzhijun@ruanzhijun.cn>
2024-06-16 18:02:52 +0800

    优化脚本

--- switch-yum-source.sh
+++ switch-yum-source.sh
@@ -2,13 +2,13 @@
 #运行例子：mkdir -p /shell && cd /shell && rm -rf switch-yum-source.sh && wget --no-check-certificate --no-cache https://raw.githubusercontents.com/share-group/shell/master/switch-yum-source.sh && sh switch-yum-source.sh
 
 rm -rf /etc/yum.repos.d && mkdir -p /etc/yum.repos.d && cd /etc/yum.repos.d && echo "[baseos]
-name=CentOS Stream $releasever - BaseOS
+name=CentOS Stream \$releasever - BaseOS
 baseurl=https://mirrors.ustc.edu.cn/centos-stream/9-stream/BaseOS/\$basearch/os/
 gpgcheck=1
 enabled=1
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
 [appstream]
-name=CentOS Stream $releasever - AppStream
+name=CentOS Stream \$releasever - AppStream
 baseurl=https://mirrors.ustc.edu.cn/centos-stream/9-stream/AppStream/\$basearch/os/
 gpgcheck=1
 enabled=1
